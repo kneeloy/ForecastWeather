127cb06e18875e1492cb39cf60f2e261a34cd5d0
Niloy Chakraborty <kneeloy@gmail.com>
2018-10-16 04:03:57 +0100

    Added  utility Loading Indicator

--- WeatherApp/WeatherApp/Utility/UIViewController+LoadingView.swift
+++ WeatherApp/WeatherApp/Utility/UIViewController+LoadingView.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIViewController {
+    class func displayLoadingIndicator(onView : UIView) -> UIView {
+        let spinnerView = UIView.init(frame: onView.bounds)
+        spinnerView.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+        let ai = UIActivityIndicatorView.init(style: .whiteLarge)
+        ai.startAnimating()
+        ai.center = spinnerView.center
+
+        DispatchQueue.main.async {
+            spinnerView.addSubview(ai)
+            onView.addSubview(spinnerView)
+        }
+
+        return spinnerView
+    }
+
+    class func removeLoadingIndicator(spinner :UIView) {
+        DispatchQueue.main.async {
+            spinner.removeFromSuperview()
+        }
+    }
+}
